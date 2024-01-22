d93449a77d0aa060abd552d1d055e52ed14b38af
Sebastian Rivera <dietmilkstudios@gmail.com>
2024-01-22 11:24:12 -0500

    [✨] Finished a basic implementation of main feats

--- rustynet/src/client.rs
+++ rustynet/src/client.rs
@@ -1,5 +1,44 @@
-// Runs the TCP client 
+// Runs the TCP client
+
+use std::net::TcpStream;
+use std::io::{self, Read, Write};
+use std::str;
+
+// Connects to the server and sends any shit from the user.
 pub fn run_client() {
-    println!("Client starting....");
+    // Trying to connect to the server at addr
+    // [*] TODO: Maybe write a fn to let the user choose a port
+    match TcpStream::connect("127.0.0.1:7878") {
+        Ok(mut stream) => {
+            print!("Connected to port 7878");
+
+            loop {
+                let mut input = String::new();
+                // Read data from stdin
+                io::stdin().read_line(&mut input).expect("Failed to read from stdin");
+                // Write data gathered to the server. Gotta do it as bytes cus YAY
+                stream.write(input.as_bytes()).expect("Failed to write to stream");
+
+                let mut buffer = [0; 512];
+                // Read servers response
+                match stream.read(&mut buffer) {
+                    Ok(_) => {
+                        // Print that data
+                        println!("Recieved from server: {}", str::from_utf8(&buffer).unwrap());
+                    },
+                    Err(e) => {
+                        // Error Handeling. Please hope to god this never has to be used. I hate
+                        // this shit
+                        println!("Failed to recieve data from: {}", e);
+                        break;
+                    }
+                }
+            }
+        },
+        // Connect failure. Just connect breh its not that hard.
+        Err(e) => {
+            println!("Failed to connect: {}", e);
+        }
+    }
     // CLIENT SHIT
 }
--- rustynet/src/main.rs
+++ rustynet/src/main.rs
@@ -25,8 +25,46 @@ fn main() {
     }
 }
 
+// Std networking libs
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::thread;
+
 // Start the TCP server
 fn run_server() {
-    println!("Server starting...");
-    // SERVER SHIT GOES HEREEEE 
+    let listener = TcpListener::bind("127.0.0.1:7878").expect("Couldnt bind :(");
+    println!("Server listening on port 7878 YAY");
+    
+    // Setup a loop for incomming connections
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                // Spawns a new thread for any new connection. I love rusts sys threads bro.
+                thread::spawn(|| {
+                    handle_client(stream);
+                });
+            }
+            Err(e) => { /* Connection faileddddd */ }
+        }
+    }
+}
+
+// Client connections 
+// @args - 'stream'
+// TCP stream between the server and the client. God help us if this doesnt work
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    // Read data from the stream in a loop
+    while match stream.read(&mut buffer) {
+        Ok(size) => {
+            // We're gonna print out anything the servers reciving
+            stream.write(&buffer[0..size]).unwrap();
+            true
+        },
+        Err(_) => { // Error handling when something ineviably fucks up <3
+            println!("Something fucked up, terminating the connection with {}", stream.perr_addr().unwrap());
+            stream.shutdown(std::net::Shutdown::Both).unwrap();
+            false
+        }
+    } {}
 }
